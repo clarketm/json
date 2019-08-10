f67541e3423a15a4ecbc0f2341e42b272dbfefdb
Travis Clarke <clarketm@google.com>
2019-08-09 21:29:53 -0700

    Support zero values of structs with omitempty: golang/go#11939

--- bench_test.go
+++ bench_test.go
@@ -329,9 +329,6 @@ func BenchmarkUnmapped(b *testing.B) {
 func BenchmarkTypeFieldsCache(b *testing.B) {
 	b.ReportAllocs()
 	var maxTypes int = 1e6
-	if testenv.Builder() != "" {
-		maxTypes = 1e3 // restrict cache sizes on builders
-	}
 
 	// Dynamically generate many new types.
 	types := make([]reflect.Type, maxTypes)
